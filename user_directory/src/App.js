4f9229d39a02322c4f8748047a7514851e9b4ce6
Adam Wenger <adamwenger44@gmail.com>
2020-03-06 14:28:50 -0500

    app and basic front end created

---
+++ user_directory/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar navbar-light bg-light">
+        <div class='col-md-12' id='head'>
+          <h1>
+            Employee Directory
+          </h1>
+          <h3>
+            Click on carrots to filter by heading or use the search box to narrow your results.
+          </h3>
+        </div>
+      </nav>
+      <br></br>
+      <nav class="navbar navbar-light bg-light">
+        <form>
+          <div class="row">
+            <div class="form">
+              <input type="text" class="form-control" placeholder="search" />
+            </div>
+          </div>
+        </form>
+      </nav>
+<br></br>
+<br></br>
+
+      <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-1">Image</div>
+        <div class="col-md-2">Name</div>
+        <div class="col-md-1">Phone</div>
+        <div class="col-md-4">Email</div>
+        <div class="col-md-1">DOB</div>
+        <div class="col-md-1"></div>
+
+
+
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
